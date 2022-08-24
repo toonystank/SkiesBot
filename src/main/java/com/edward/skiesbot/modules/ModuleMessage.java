8ba25a32cbaf57595cfd802b4d3a5a04ded96283
ToonyStank <hakeemiqbalnew@gmail.com>
2022-08-25 00:30:53 +0530

    `Changes` Module message

--- src/main/java/com/edward/skiesbot/modules/ModuleMessage.java
+++ src/main/java/com/edward/skiesbot/modules/ModuleMessage.java
@@ -2,8 +2,8 @@ package com.edward.skiesbot.modules;
 
 import com.edward.skiesbot.SkiesBot;
 import com.edward.skiesbot.utils.PlaceholderParser;
-import com.edward.skiesbot.utils.enums.ModuleDataType;
 import com.edward.skiesbot.utils.enums.ModuleOptions;
+import com.taggernation.taggernationlib.config.ConfigManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -11,21 +11,28 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.requests.RestAction;
 
+import java.io.IOException;
+
 public class ModuleMessage {
 
     private final Modules modules;
     private String message;
     private final boolean isEnabled;
-    private String channelID;
+    private final ModuleOptions moduleOptions;
+    private String value;
     private final JDA jda;
+    private boolean log;
+    private ConfigManager logConfig;
 
 
-    public ModuleMessage(Modules modules, String message, boolean isEnabled, String channelID) {
+    public ModuleMessage(Modules modules, boolean isEnabled, ModuleOptions moduleOptions, String value,String moduleName , boolean log) throws IOException {
         this.modules = modules;
-        this.message = message;
+        this.message = this.getMessageConfig();
         this.isEnabled = isEnabled;
-        this.channelID = channelID;
+        this.moduleOptions = moduleOptions;
+        this.value = value;
         this.jda = SkiesBot.getInstance().getJda();
+        if (log) this.logConfig = new ConfigManager(SkiesBot.getInstance(), moduleName + "_log.yml", "Data", false, false);
     }
     public String getMessage() {
         return message;
@@ -33,24 +40,35 @@ public class ModuleMessage {
     public boolean isEnabled() {
         return isEnabled;
     }
-    public String getChannelID() {
-        return channelID;
+    public String getValue() {
+        return value;
     }
     public void initialize(MessageReceivedEvent event) {
         this.message = PlaceholderParser.parse(event.getMessage().getContentRaw(), event);
-        this.channelID = event.getChannel().getId();
-        this.finalize(message);
+        this.value = event.getChannel().getId();
+        if (moduleOptions.equals(ModuleOptions.CHANNEL)) this.finalize(message, this.value);
+        else if (moduleOptions.equals(ModuleOptions.MESSAGE)) this.finalize(message, event.getChannel().getId());
+        // Module option mention role is not done
+        if (log) saveToYML(message, event.getAuthor().getId(), event.getMessageId());
     }
     public void initialize(GuildMemberJoinEvent event) {
         this.message = PlaceholderParser.parse(message, event);
-        this.finalize(message);
+        this.finalize(message, this.value);
     }
-    public void finalize(String message) {
+    public void finalize(String message, String channelID) {
         if (!isEnabled) return;
         TextChannel channel = jda.getTextChannelById(channelID);
         if (channel == null) return;
         RestAction<Message> action = channel.sendMessage(message);
         Message restMessage = action.complete();
         modules.setTextMessage(restMessage);
+    }
+    public String getMessageConfig() {
+        return modules.getString("data.Message.Message");
+    }
+    public void saveToYML(String message, String author, String messageID) {
+        logConfig.set("Message." + messageID + ".TEXT", message);
+        logConfig.set("Message." + messageID + ".AUTHOR", author);
+        logConfig.save();
     }
 }
